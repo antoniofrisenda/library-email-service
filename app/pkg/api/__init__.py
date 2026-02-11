1f8ff8a927a043cbd2871022e04923695503b6c8
AndreaPetracca <andrea.petracca@vidyasoft.it>
2026-02-11 09:56:57 +0100

    v5 refactor

--- app/pkg/api/__init__.py
+++ app/pkg/api/__init__.py
@@ -1,3 +1,3 @@
-from app.pkg.api.v1.routers.email_controller_route import router, _get_service as Instance
+from app.pkg.api.v1.routers.email_controller import router, _get_service as Instance
 
 __all__ = ["router", "Instance"]
--- app/pkg/service/__init__.py
+++ app/pkg/service/__init__.py
@@ -1,4 +1,4 @@
+from app.pkg.service.email_service import MailerService as Service
 from app.pkg.service.email_sqs_consumer import SQSConsumer as Consumer
-from app.pkg.service.email_sender_service import MailerService as Service
 
 __all__ = ["Service", "Consumer"]
